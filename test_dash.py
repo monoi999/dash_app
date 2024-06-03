c3afaa19c27fdca3249526c713c39f28b7540dab
monoi999 <137073851+monoi999@users.noreply.github.com>
2024-06-03 09:51:31 +0900

    Update test_dash.py
    test dash gapminder

--- test_dash.py
+++ test_dash.py
@@ -1,17 +1,31 @@
 # [4-1]
 
-from dash import Dash, html
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+from dash import Dash, html, dcc, callback, Input, Output, State
+import dash_bootstrap_components as dbc
+import plotly.express as px
+df = px.data.gapminder()
 
-app = Dash(__name__)
+yearList = df['year'].sort_values().unique()
+continentList = df['continent'].unique()
+marks = { str(year):str(year) for year in yearList }
 
-app.layout = html.Div(children=[
-    html.Div(children='Exam Div'),
-    html.Br(),
-    html.Div(children=[
-        html.H1(children='Exam H1'),
-        html.P(children='Exam P')
-    ])
-])
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
+
+app.layout = html.Div([
+    dcc.Dropdown(options=continentList, id='continent'),
+    dcc.Slider(yearList[0], yearList[-1], step=None, marks=marks, id='year'),
+    html.Hr(),
+    html.Div(id='result')
+], className='dbc w-25 d-grid gap-2 m-2 w-25 h5')
+
+@callback(Output('result','children'),
+          Input('continent','value'), Input('year','value'))
+def func(continent, year):
+    print(continent, year)
+    if continent and year :
+        popSum = df.query('continent==@continent and year==@year')['pop'].sum()
+        return f'Total {continent} Population in {year} => {popSum}'
 
 if __name__ == '__main__':
-    app.run_server(debug=True) 
+    app.run_server(debug=True)
