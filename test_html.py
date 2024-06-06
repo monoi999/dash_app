a1f9cb565222de0510405c939bfb0c5e07932297
monoi999 <137073851+monoi999@users.noreply.github.com>
2024-06-06 16:07:16 +0900

    Update test_html.py

--- test_html.py
+++ test_html.py
@@ -1,17 +1,1 @@
 
-from dash import Dash, html
-
-app = Dash(__name__)
-server = app.server
-
-app.layout = html.Div(children=[
-    html.Div(children='Exam Div'),
-    html.Br(),
-    html.Div(children=[
-        html.H1(children='Exam H1'),
-        html.P(children='Exam P')
-    ])
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True) 
